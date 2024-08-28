009690330eb8e02c60bb8d6506fce99c90067be2
Alarih1212 <alarih1212@gmail.com>
2024-08-28 12:20:34 +0300

    first commit

---
+++ python_trening/task_2_string.py
@@ -0,0 +1,19 @@
+s = "Эта строка"
+print(s + '\n')
+g = '''Многострочная
+строка'''
+print(g + '\n')
+print('Строки ' + 'можно ' + 'складывать')
+
+print('Auto' * 3)
+print(len('test'))
+
+
+a = 'Automation'
+
+print(a[0])
+print(a[1])
+print(a[-2])
+print(a[1:3])
+
+
